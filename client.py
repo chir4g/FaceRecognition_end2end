f810837490fbd5424b166975e11ba48c12d92fa5
iamchiragsharma <chiragsharmagtbit@outlook.com>
2019-09-14 20:39:26 +0530

    Done

--- client.py
+++ client.py
@@ -2,7 +2,8 @@ import requests
 import json
 import cv2
 
-#local = "http://0.0.0.0:5000"
+local = "http://0.0.0.0:5000"
+globalhost = "http://103.212.144.244:5000"
 addr = 'https://webandroidtest.herokuapp.com'
 test_url = addr + '/imageprocess'
 
